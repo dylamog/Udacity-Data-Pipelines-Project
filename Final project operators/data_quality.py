24a2448cf716c7fa56ad8916173262343be3a7f9
dylamog <145466665+dylamog@users.noreply.github.com>
2023-12-31 21:51:22 +0200

    Update data_quality.py

--- Final project operators/data_quality.py
+++ Final project operators/data_quality.py
@@ -7,27 +7,24 @@ class DataQualityOperator(BaseOperator):
     ui_color = '#89DA59'
 
     @apply_defaults
-    def __init__(self, redshift_conn_id="", tables=[], *args, **kwargs):
+    def __init__(self, redshift_conn_id="", tests=None, *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
         
         self.redshift_conn_id = redshift_conn_id
-        self.tables = tables
+        self.tests = tests or []
 
     def execute(self, context):
         
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
 
-        for table in self.tables:
-            records = redshift.get_records("SELECT COUNT(*) FROM {}".format(table))  
-            
-            if len(records) < 1 or len(records[0]) < 1:
-                self.log.error("{} returned no results".format(table))
-                raise ValueError("Data quality check failed. {} returned no results".format(table))
-                
-            num_records = records[0][0]
-            if num_records == 0:
-                self.log.error("No records present in destination table {}".format(table))
-                raise ValueError("No records present in destination {}".format(table))
-                
-            self.log.info("Data quality on table {} check passed with {} records".format(table, num_records))
+        for test in self.tests:
+            check_sql = test['check_sql']
+            expected_result = test['expected_result']
+
+            self.log.info(f"Running SQL: {check_sql}")
+            records = redshift.get_records(check_sql)
+            result = records[0][0]
+
+            if result != expected_result:
+                raise ValueError(f"Data quality check failed. Expected result: {expected_result}, Actual result: {result}")
